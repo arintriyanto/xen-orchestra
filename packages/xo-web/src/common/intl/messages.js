d1166e34e6b872075b70baae31ba52871e4efa31
BARHTAOUI <rajaa.barhtaoui@gmail.com>
2021-10-04 10:50:35 +0200

    feat(xo-web/menu): notify user when proxies need to be upgraded

--- packages/xo-web/src/common/intl/messages.js
+++ packages/xo-web/src/common/intl/messages.js
@@ -2372,6 +2372,7 @@ const messages = {
   proxyRunningBackupsMessage:
     'The upgrade will interrupt {nJobs, number} running backup job{nJobs, plural, one {} other {s}}. Do you want to continue?',
   upgradeNeededForProxies: 'Some proxies need to be upgraded. Click here to get more information.',
+  notificationUpgradeNeededForProxies: 'Some proxies need to be upgraded.',
 
   // ----- Utils -----
   secondsFormat: '{seconds, plural, one {# second} other {# seconds}}',
